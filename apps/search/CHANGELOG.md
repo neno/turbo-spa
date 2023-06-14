6e982170a6d361a0e0df064fbc8bb67a822aebf6
Nenad Brcic <neno@neno.ch>
2023-06-14 16:55:43 +0200

    Enable lib build

---
+++ apps/search/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @nenoweb/search
+
+## 0.1.0
+
+### Minor Changes
+
+- Inital package creation
--- apps/search/vite.config.ts
+++ apps/search/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+      skipDiagnostics: false,
+    })
+  ],
+  build: {
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'search',
+      formats: ['es'],
+      fileName: (format) => `search.${format}.js`
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  }
 })
